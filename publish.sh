462b13b1f6c35a698485bd2fd4958ce06432acf0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2020-08-12 12:29:43 +0200

    cleaned up old ci, added token based autho for pypi

--- publish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run -it --rm -v `pwd`:/io quay.io/pypa/manylinux2010_x86_64 /io/ci/build_wheels.sh && \
-python3 setup.py sdist && \
-python3 -m twine upload --repository testpypi dist/* wheelhouse/*
